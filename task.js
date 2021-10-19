177a556cf6fe3a59975f7ef86f20de5cede1fad2
Jalfaro1010 <Joshua.alfaro@gmail.com>
2021-10-19 11:00:47 -0400

    Added Initial prompt menu.

---
+++ task.js
@@ -0,0 +1,13 @@
+// Task Manager Project
+
+const menu = `TASK MANAGER
+
+What would you like to do (Please enter one of the options below):
+"TASKS" - Display All Tasks
+"NEW" - Add A New Task
+"REMOVE" - Remove A Task
+"CLOSE" - Close The Task Manager
+`;
+
+// Displays Menu For User
+let userInput = prompt(menu);
